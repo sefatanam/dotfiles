ad206e45c2527e49c6b78ae6f20af7783ff59272
sefatanam <sefatanam@gmail.com>
2025-03-24 21:49:10 +0600

    refactor(nvim): update nvim configurations

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -14,3 +14,5 @@ vim.opt.tabstop = 2 -- Number of spaces a tab counts for
 vim.opt.softtabstop = 2 -- Number of spaces a tab key inserts
 vim.opt.list = true
 vim.opt.listchars:append("space:.")
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = false
