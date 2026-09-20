924fddfcfa255dc588d3612dbe27cdfeb6ed3606
Sefat Anam <sefatanam@gmail.com>
2026-09-20 15:58:22 +0600

    feat(omp-nvim): add keymap to open omp terminal

--- nvim/lua/plugins/omp-nvim.lua
+++ nvim/lua/plugins/omp-nvim.lua
@@ -1,11 +1,21 @@
 return {
   "l3aro/omp.nvim",
   version = "*", -- Latest stable release
+  dependencies = { "folke/snacks.nvim" },
   -- Disable the default <leader>oa maps; init runs before the plugin loads
   init = function()
     vim.g.omp_nvim_no_keymaps = true
   end,
   keys = {
+    -- omp itself is a separate process; this plugin only bridges to an
+    -- already-running instance, so open it in a terminal first.
+    {
+      "<leader>oo",
+      function()
+        Snacks.terminal({ "omp" }, { cwd = LazyVim.root() })
+      end,
+      desc = "omp: open (Root Dir)",
+    },
     { "<leader>oa", "<Cmd>OmpAsk<CR>", desc = "omp: ask" },
     {
       "<leader>oa",
